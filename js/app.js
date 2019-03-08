e776b2d5e4d4c1bbd94bd41f122ae5a97212053a
ngurajeka <ngurajeka.92@gmail.com>
2019-03-08 17:18:13 +0700

    add more info to glossary

--- js/app.js
+++ js/app.js
@@ -5,6 +5,18 @@ let data = [
     },{
         "title": "AUM (Asset Under Management)",
         "description": "Asset Under Management (AUM) menunjukkan seberapa besar dana masyarakat yang terkumpul pada suatu dana pensiun",
+    },{
+        "title": "DPPK",
+        "description": "DPPLK adalah Dana Pensiun yang dibentuk / didirikan oleh orang atau badan yang mempekerjakan karyawan, selaku Pendiri dari Dana Pensiun, untuk menyelenggarakan Program Pensiun Manfaat Pasti atau Program Pensiun Iuran Pasti bagi kepentingan sebagian atau keseluruhan karyawannya yang menjadi peserta program pensiun dan yang menimbulkan kewajiban terhadap pemberi kerja",
+    },{
+        "title": "DPLK",
+        "description": "DPLK adalah Dana Pensiun yang dibentuk oleh Bank atau perusahaan asuransi jiwa untuk menyelenggarakan Program Pensiun Iuran Pasti bagi perorangan, baik karyawan maupun pekerja mandiri yang terpisah dari Dana Pensiun Pemberi Kerja bagi karyawan bank atau perusahaan asuransi jiwa yang bersangkutan"
+    },{
+        "title": "PPMP (Program Pensiun Manfaat Pasti)",
+        "description": "PPMP adalah program pensiun yang manfaatnya ditetapkan dalam PDP atau program pensiun lain yang bukan merupakan PPIP"
+    },{
+        "title": "PPIP (Program Pensiun Iuran Pasti)",
+        "description": "PPIP adalah program pensiun yang iurannya ditetapkan dalam PDP dan seluruh iuran serta hasil pengembangannya dibukukan pada rekening masing-masing peserta sebagai manfaat pensiun"
     }
 ];
 
